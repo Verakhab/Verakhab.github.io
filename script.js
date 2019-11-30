f06d43a80661a4e2eb268add8c07e8034da0e054
Denis Bakharev <denis.bahareff@yandex.ru>
2019-11-30 17:38:36 +0300

    amend work

--- script.js
+++ script.js
@@ -3,17 +3,64 @@ const nameInput = form.elements.name;
 const emailInput = form.elements.email;
 const telInput = form.elements.tel;
 const urlInput = form.elements.url;
-
-const regexName = /(^[А-Я]{1}[а-яё]+|^[А-Я]{1}[а-яё]+\-^[А-Я]{1}[а-яё]+)/;
-const regexNEmail = /\w{2,}@[A-Z]+[a-z]+\.[A-Z]+[a-z]{2}]/;
-const regexTel = /\+?\d{1}\(\d{3}\)\s?\-?\d{3}\s?\-?\d{2}\s?\-?\d{2}/;
-const regexUrl = /(http\:\/{1}\/{1}|https\:\/{1}\/{1})w{0,3}\.?\:?\d{0,5}(\d{3}\.\d{3}\.\d{3}\.\d{3}|stasbasov\.ru)\#?/;
+debugger;
+console.log(nameInput.value);
+const regexName = /^([А-Я][а-яё]+)(-)?([А-Я]?[а-яё]*)?$/;
+const regexEmail = /^([\dA-Za-z\.-]{2,})+@([A-Za-z\.-]{2,})+\.([A-Za-z]{2,6})$/;
+const regexTel = /^(\+)?(\d)\s?(\()?(\d{3})(\))?\s?-?(\d{3})\s?-?(\d{2})\s?-?(\d{2})$/;
+const regexUrl = /^(http(s)?:\/\/)(w{3}\.)?(\d*\.\d*\.\d*\.\d*)?(([A-Za-z\.-]{2,})\.([A-Za-z]{2,6}))?(:\d{2,5})?\/?([\dA-Za-z\/]+#?)?$/;
 
 function validate() {
     nameInput.setAttribute('pattern', `${regexName}`);
-    emailInput.setAttribute('pattern', `${regexNEmail}`);
+    emailInput.setAttribute('pattern', `${regexEmail}`);
     telInput.setAttribute('pattern', `${regexTel}`);
     urlInput.setAttribute('pattern', `${regexUrl}`);
 } 
 
 validate();
+
+nameInput.addEventListener('input', event => {
+    console.log(nameInput.value);
+        function matchRegExp() {debugger;
+            let name = nameInput.value;
+            name.match(regexName);
+            console.log(name.match(regexName));
+            console.log(name.length);
+        }
+        return matchRegExp();
+});
+
+emailInput.addEventListener('input', event => {
+    console.log(emailInput.value);
+        function matchRegExp() {debugger;
+            let email = emailInput.value;
+            email.match(regexEmail);
+            console.log(email.match(regexEmail));
+            console.log(email.length);            
+        }
+        return matchRegExp();
+});
+
+telInput.addEventListener('input', event => {
+    console.log(telInput.value);
+        function matchRegExp() {debugger;
+            let tel = telInput.value;
+            tel.match(regexTel);
+            console.log(tel.match(regexTel));
+            console.log(tel.length);            
+        }
+        return matchRegExp();
+});
+
+urlInput.addEventListener('input', event => {
+    console.log(urlInput.value);
+        function matchRegExp() {debugger;
+            let url = urlInput.value;
+            url.match(regexUrl);
+            console.log(url.match(regexUrl));
+            console.log(url.length);            
+        }
+        return matchRegExp();
+});
+
+// const match = matchRegExp();
